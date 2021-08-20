3901723dd8915c9f9e45eff64ecafe529b652df2
NehaHooda272 <68366394+NehaHooda272@users.noreply.github.com>
2021-08-20 14:01:22 +0530

    Web Calculator with unit test

---
+++ WebCalculator/net.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"WebCalculator/calculator"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type data struct {
+	Exp string
+	Res string
+}
+
+func HomePage() {
+	// fs := http.FileServer(http.Dir("./static"))
+	// http.Handle("/", fs)
+	http.HandleFunc("/evaluate", Calculate)
+	http.HandleFunc("/", First)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func Calculate(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	exp := r.Form.Get("exp")
+	res, err := calculator.CalculatorResult(string(exp))
+	t, err2 := template.ParseFiles("static/result.html")
+	if err2 != nil {
+		panic(err)
+	}
+	var d1 data
+	if err == nil {
+		d1 = data{Exp: string(exp), Res: fmt.Sprintf("%f", res)}
+	} else {
+		d1 = data{Exp: string(exp), Res: err.Error()}
+	}
+
+	err = t.Execute(w, d1)
+	// http.ServeFile(w, r, "static/result.html")
+}
+func First(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
+}
+func main() {
+	HomePage()
+
+}
